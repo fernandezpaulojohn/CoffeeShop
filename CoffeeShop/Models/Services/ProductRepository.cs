c20fb9ae870dfcba139aced9f1c21e57ef0ff78f
unknown <fernandezpaulojohn@gmail.com>
2023-10-11 15:33:46 +0800

    Implement Repository Pattern

--- CoffeeShop/Models/Services/ProductRepository.cs
+++ CoffeeShop/Models/Services/ProductRepository.cs
@@ -8,23 +8,23 @@ namespace CoffeeShop.Models.Services
         {
             new Product { Id= 1, Name = "Americano", Price = 25, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
 
-            new Product { Id= 1, Name = "Americano", Price = 25, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
+            new Product { Id= 2, Name = "Mocha", Price = 30, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
 
-            new Product { Id= 1, Name = "Americano", Price = 25, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
+            new Product { Id= 3, Name = "Cafe au Lait", Price = 35, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
         };
         public IEnumerable<Product> GetAllProducts()
-        { 
-            throw new NotImplementedException();
+        {
+            return ProductsList;
         }
 
         public Product GetProductDetail(int id)
         {
-            throw new NotImplementedException();
+            return ProductsList.FirstOrDefault(p => p.Id == id);
         }
 
         public IEnumerable<Product> GetTrendingProducts()
         {
-            throw new NotImplementedException();
+            return ProductsList.Where(p => p.IsTrendingProduct);
         }
     }
 }
