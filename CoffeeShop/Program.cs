3cca7cf25e035273629ac8ff2453591266f05f50
unknown <fernandezpaulojohn@gmail.com>
2023-10-11 16:37:14 +0800

    Register Services in IOC Container

--- CoffeeShop/Program.cs
+++ CoffeeShop/Program.cs
@@ -1,8 +1,11 @@
+using CoffeeShop.Models.Interfaces;
+using CoffeeShop.Models.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
