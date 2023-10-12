ffda5fad5f6014fc72186cdfff7860cb7327abf4
unknown <fernandezpaulojohn@gmail.com>
2023-10-13 04:04:51 +0800

    DbContext and DBSet

---
+++ CoffeeShop/Data/CoffeeShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CoffeeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.Data
+{
+    public class CoffeeShopDbContext: DbContext
+    {
+        public CoffeeShopDbContext(DbContextOptions<CoffeeShopDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
