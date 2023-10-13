c734181be6b3c0fb301abbad3fc5058892cf10be
unknown <fernandezpaulojohn@gmail.com>
2023-10-14 02:28:07 +0800

    Seed Data

--- CoffeeShop/Migrations/20231013182332_SeedProductsTable.Designer.cs
+++ CoffeeShop/Migrations/20231013182332_SeedProductsTable.Designer.cs
@@ -3,6 +3,7 @@ using CoffeeShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoffeeShop.Migrations
 {
     [DbContext(typeof(CoffeeShopDbContext))]
-    partial class CoffeeShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231013182332_SeedProductsTable")]
+    partial class SeedProductsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
