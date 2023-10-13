3094c240cd58f69101cfb832a1a3a4176cfbf465
unknown <fernandezpaulojohn@gmail.com>
2023-10-13 22:53:21 +0800

    Modify Repository Class

--- CoffeeShop/Models/Services/ProductRepository.cs
+++ CoffeeShop/Models/Services/ProductRepository.cs
@@ -1,30 +1,28 @@
-﻿using CoffeeShop.Models.Interfaces;
+﻿using CoffeeShop.Data;
+using CoffeeShop.Models.Interfaces;
 
 namespace CoffeeShop.Models.Services
 {
     public class ProductRepository : IProductRepository
     {
-        private List<Product> ProductsList = new List<Product>()
+        private CoffeeShopDbContext dbContext;
+        public ProductRepository(CoffeeShopDbContext dbContext)
         {
-            new Product { Id= 1, Name= "Americano", Price= 25, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
-
-            new Product { Id= 2, Name= "Mocha", Price= 30, Detail ="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
-
-            new Product { Id= 3, Name= "Cafe au Lait", Price= 35, Detail="Americano\r\nThe Americano coffee is a classic espresso-based drink that is simple yet satisfying. It is made by adding hot water to a shot of espresso, which dilutes the intensity and results in a rich, bold coffee with a smooth finish. This versatile drink can be enjoyed on its own or with a splash of cream, making it a popular choice for coffee lovers everywhere. Whether you need a pick-me-up in the morning or a midday boost, the Americano is a dependable choice that never fails to deliver.", ImageUrl="https://res.cloudinary.com/durcypdqc/image/upload/v1675704066/Coffee%20Shop%20Asp%20.NET%20Core%20Assets/americano_gorkrx.png\r\n"},
-        };
+            this.dbContext = dbContext;
+        }
         public IEnumerable<Product> GetAllProducts()
         {
-            return ProductsList;
+            return dbContext.Products;
         }
 
-        public Product GetProductDetail(int id)
+        public Product? GetProductDetail(int id)
         {
-            return ProductsList.FirstOrDefault(p => p.Id == id);
+            return dbContext.Products.FirstOrDefault(p => p.Id == id);
         }
 
         public IEnumerable<Product> GetTrendingProducts()
         {
-            return ProductsList.Where(p => p.IsTrendingProduct);
+            return dbContext.Products.Where(p => p.IsTrendingProduct);
         }
     }
 }
