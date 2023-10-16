2e56297139abbfa72b9155530f0368e415f2f55c
unknown <fernandezpaulojohn@gmail.com>
2023-10-16 19:05:08 +0800

    Populate Trending Products Data

--- CoffeeShop/Controllers/HomeController.cs
+++ CoffeeShop/Controllers/HomeController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoffeeShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoffeeShop.Controllers
 {
     public class HomeController : Controller
     {
+        private IProductRepository productRepository;
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+       
         public IActionResult Index()
         {
-            return View();
+            return View(productRepository.GetTrendingProducts());
         }
     }
 }
