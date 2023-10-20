ca0f6cc499b0efe3b461f4c190741618d5690e51
Paulo John Fernandez <fernandezpaulojohn@gmail.com>
2023-10-20 20:59:02 +0800

    add

--- CoffeeShop/Data/CoffeeShopDbContext.cs
+++ CoffeeShop/Data/CoffeeShopDbContext.cs
@@ -11,7 +11,7 @@ namespace CoffeeShop.Data
         }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
