0bfe4536f824840a995d6369716f819bc5c2d0c3
Paulo John Fernandez <fernandezpaulojohn@gmail.com>
2023-10-24 00:09:00 +0800

    Implement Migrations to add order table.

---
+++ CoffeeShop/Migrations/20231023160705_OrderAdded.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Products_PropertyId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.RenameColumn(
+                name: "ShoppingCartID",
+                table: "ShoppingCartItems",
+                newName: "ShoppingCartId");
+
+            migrationBuilder.RenameColumn(
+                name: "PropertyId",
+                table: "ShoppingCartItems",
+                newName: "ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartItems_PropertyId",
+                table: "ShoppingCartItems",
+                newName: "IX_ShoppingCartItems_ProductId");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrderTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderPlaced = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Products_ProductId",
+                table: "ShoppingCartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Products_ProductId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "ShoppingCartId",
+                table: "ShoppingCartItems",
+                newName: "ShoppingCartID");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "ShoppingCartItems",
+                newName: "PropertyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartItems_ProductId",
+                table: "ShoppingCartItems",
+                newName: "IX_ShoppingCartItems_PropertyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Products_PropertyId",
+                table: "ShoppingCartItems",
+                column: "PropertyId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
--- CoffeeShop/Program.cs
+++ CoffeeShop/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
 builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>(ShoppingCartRepository.GetCart);
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 builder.Services.AddDbContext<CoffeeShopDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopDbContextConnection")));
 builder.Services.AddSession();
